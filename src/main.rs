db8cf733105f50015fe8a24487c4d277c80c561f
bhavin sojitra <122201342+c0d3-dump@users.noreply.github.com>
2023-04-17 06:25:48 +0000

    created parser to read query and get all variables and replace query string with ? for sqlite and mysql

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ mod parser;
 mod tui;
 
 fn main() {
-    tui::run()
+    // tui::run()
+
+    let q = "SELECT * FROM user WHERE email=${email} and password=${password:'1234'}";
+
+    let (_, out) = parser::parse_query(q).unwrap();
+    println!("{:#?}", out);
+
+    let qout = parser::replace_variables_in_query(q, out);
+
+    println!("{:#?}", qout);
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,2 +1,52 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_until},
+    character::complete::{alpha1, alphanumeric1, multispace1},
+    combinator::{peek, value},
+    multi::many0,
+    sequence::{delimited, preceded},
+    IResult,
+};
 
+fn parse_identifier(input: &str) -> IResult<&str, &str> {
+    preceded(peek(alpha1), take_until("}"))(input)
+}
 
+fn parse_variable(input: &str) -> IResult<&str, &str> {
+    delimited(tag("${"), parse_identifier, tag("}"))(input)
+}
+
+fn parse_special_character(input: &str) -> IResult<&str, &str> {
+    alt((
+        tag("*"),
+        tag("&"),
+        tag("$"),
+        tag("?"),
+        tag("("),
+        tag(")"),
+        tag("="),
+    ))(input)
+}
+
+pub fn parse_query(input: &str) -> IResult<&str, Vec<&str>> {
+    let (input, variables) = many0(alt((
+        parse_variable,
+        value("", alphanumeric1),
+        value("", parse_special_character),
+        value("", multispace1),
+    )))(input)?;
+
+    let variables = variables.into_iter().filter(|v| v != &"").collect();
+
+    Ok((input, variables))
+}
+
+pub fn replace_variables_in_query(input: &str, variables: Vec<&str>) -> String {
+    let mut out = input.to_string();
+
+    variables.into_iter().for_each(|var| {
+        let from = format!("${{{var}}}");
+        out = out.replace::<&str>(from.as_ref(), "?");
+    });
+    out
+}
