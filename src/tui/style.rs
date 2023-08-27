8d5ea88e558333364dce3392c9efaab5dd676fd0
c0d3-dump <c0d3.dump@gmail.com>
2023-08-27 23:58:24 +0530

    fixed theme

--- src/tui/style.rs
+++ src/tui/style.rs
@@ -1,4 +1,4 @@
-use cursive::theme::{BorderStyle, Palette, Theme};
+use cursive::theme::{BaseColor, BorderStyle, Color, Palette, PaletteColor, Theme};
 
 pub fn get_theme() -> Theme {
     let mut theme = Theme::default();
@@ -6,9 +6,21 @@ pub fn get_theme() -> Theme {
     theme.shadow = false;
     theme.borders = BorderStyle::Simple;
 
-    let palette = Palette::default();
+    let mut palette = Palette::default();
 
-    // palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::Shadow] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
+
+    palette[PaletteColor::Primary] = Color::Dark(BaseColor::White);
+    palette[PaletteColor::Secondary] = Color::parse("#c1c1c1").unwrap();
+    palette[PaletteColor::Tertiary] = Color::Dark(BaseColor::Black);
+
+    palette[PaletteColor::TitlePrimary] = Color::Dark(BaseColor::Blue);
+    palette[PaletteColor::TitleSecondary] = Color::Dark(BaseColor::Yellow);
+
+    palette[PaletteColor::Highlight] = Color::Dark(BaseColor::Magenta);
+    palette[PaletteColor::HighlightInactive] = Color::parse("#5555FF").unwrap();
 
     theme.palette = palette;
 
